98b05d2fb03b9bfbcba0ea9f43c913f165369834
leeroywking <leeroywking@gmail.com>
2019-11-13 12:54:44 -0800

    added route for peopleObject

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 const jsonObject = { hello: 'world' };
+const userObj = require('./userObj.json');
 
 function reply(req, res) {
-  res.send(JSON.stringify(jsonObject));
+  res.send(jsonObject);
+}
+
+function peopleHandler(req, res) {
+  res.send(userObj);
 }
 
 app.get('/', reply);
+app.get('/peopleObject', peopleHandler);
+app.post('/peopleObject', peopleHandler);
 
 app.listen(PORT);

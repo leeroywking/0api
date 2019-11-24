a1f73f8f55632cf6f3c25cffcd4252993b67ecd4
Lee-Roy King <leeroywking@gmail.com>
2019-11-24 03:21:29 -0800

    added autocomplete endpoint

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const masterTree = require('./autocomplete/wordTree.js');
+
 
 const jsonObject = { hello: 'world' };
 const userObj = require('./userObj.json');
@@ -15,8 +17,16 @@ function peopleHandler(req, res) {
   res.send(userObj);
 }
 
+function autocomplete(req,res){
+  let partialWord = req.params.partial;
+  let limit = req.params.limit;
+  let wordList = masterTree.returnWordsFromPartial(partialWord, limit)
+  res.send({partialWord, limit, wordList})
+}
+
 app.get('/', reply);
 app.get('/peopleObject', peopleHandler);
 app.post('/peopleObject', peopleHandler);
+app.get('/autocomplete/:partial/:limit', autocomplete);
 
 app.listen(PORT);
